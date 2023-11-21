18630c1eae2bbd805a75cc8fbe66d38c612fc47d
Sarath <vssarathc04@gmail.com>
2023-11-21 13:06:31 +0530

    automatic creation of the upload folder

--- const.py
+++ const.py
@@ -6,4 +6,9 @@ import os
 IMAGE_SIZE = (224, 224)
 MAX_LENGTH = 34
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
+
+# check if the folder exists if not create it 
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
